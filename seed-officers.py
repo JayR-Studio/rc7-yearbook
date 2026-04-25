65363048d6d21f9ae97622160b19449de6b27a31
Joseph Samuel Gwanto <jayr431722@gmail.com>
2026-04-25 16:36:33 +0100

    Added Favicon

--- seed-officers.py
+++ seed-officers.py
@@ -2,6 +2,7 @@ from app import app, preload_officers, Officers
 
 
 with app.app_context():
+    print("DATABASE:", app.config["SQLALCHEMY_DATABASE_URI"])
     if Officers.query.first() is None:
         preload_officers()
 
